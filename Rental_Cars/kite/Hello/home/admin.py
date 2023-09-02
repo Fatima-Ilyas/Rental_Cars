0db5bdcdcf1fe4861caa866204ee97da8e973a92
Fatima-Ilyas <ilyasfatima93@gmail.com>
2023-09-02 11:28:16 +0500

    Rental_Cars

---
+++ Rental_Cars/kite/Hello/home/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from home.models import Contact
+from home.models import booking
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(booking)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
